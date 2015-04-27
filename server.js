b23837e41d2244a95918bd38ef53dbc750c2dde8
nicodel <nicolas.delebecque@gmail.com>
2015-04-27 16:05:07 +0200

    cleaning

--- server.js
+++ server.js
@@ -1,29 +1,5 @@
 /* jslint strict: true, node: true*/
 
-// var http = require('http');
-// var url = require('url');
-// var fs = require('fs');
-
-/* OpenClassRoom example
- */
-/*var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Salut tout le monde');
-});
-server.listen(8080);*/
-
-/* Mixu node.js book
- */
-/*http.createServer(function(req, res) {
-  fs.readFile('./index.html', function(err, data) {
-    res.end(data);
-  });
-  // res.end('hello world');
-}).listen(8080, 'localhost');
-console.log('Server running');*/
-
-/* Espress Hello world example
- */
 var express = require('express');
 var app = express();
 
