6290230ce97fba486b516b809026db059022436b
nicodel <nicolas.delebecque@gmail.com>
2015-04-27 15:04:21 +0200

    adding Express example

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 /* jslint strict: true, node: true*/
 
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+// var http = require('http');
+// var url = require('url');
+// var fs = require('fs');
 
 /* OpenClassRoom example
  */
@@ -14,10 +14,26 @@ server.listen(8080);*/
 
 /* Mixu node.js book
  */
-http.createServer(function(req, res) {
+/*http.createServer(function(req, res) {
   fs.readFile('./index.html', function(err, data) {
     res.end(data);
   });
   // res.end('hello world');
 }).listen(8080, 'localhost');
-console.log('Server running');
+console.log('Server running');*/
+
+/* Espress Hello world example
+ */
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('hello express world');
+});
+
+var server = app.listen(8080, function(req, res) {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s',  host, port);
+});
