215ee891006435539188b5d3825a651b7382df70
nicodel <nicolas.delebecque@gmail.com>
2015-04-28 19:11:13 +0200

    step 3

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+/* jslint strict: true, node: true */
 // server.js
 
 // Generate a new instance of express server.
-var express = require('express')
-  , http = require('http');
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -21,19 +23,32 @@ var server = http.createServer(app).listen(port, host, function() {
    render templates */
 app.set('views', __dirname + '/public');
 app.engine('.html', require('jade').__express);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Let's define some bookmarks
-var bookmarks = []
+var bookmarks = [];
 bookmarks.push({title: "Cozycloud", url: "https://cozycloud.cc"});
 bookmarks.push({title: "Cozy.io", url: "http://cozy.io"});
 bookmarks.push({title: "My Cozy", url: "http://localhost:9104/"});
 
 // We render the templates with the data
 app.get('/', function(req, res) {
-  params = {
+  var params = {
     "bookmarks": bookmarks
-  }
+  };
   res.render('index.jade', params, function(err, html) {
     res.send(html);
   });
+});
+
+// We define a new route that will handle bookmark creation
+app.post('/add', function(req, res) {
+  bookmarks.push(req.body);
+  res.redirect('/');
+});
+
+// We define another route that will handle bookmark deletion
+app.get('/delete/:id', function(req, res) {
+  bookmarks.splice(req.params.id, 1);
+  res.redirect('/');
 });
