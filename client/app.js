69862145656962125340292c2a3789826bdc9ed9
nicodel <nicolas.delebecque@gmail.com>
2015-04-30 00:07:03 +0200

    plane

--- client/app.js
+++ client/app.js
@@ -5,10 +5,50 @@
   var PouchDB = require('pouchdb');
   var db = new PouchDB('cozy-pouch-client');
 
-  document.getElementById('btn-add').addEventListener('click', function(el) {
-    console.log('add button clicked', el);
-  });
-  document.getElementById('add-form').onsubmit = function() {
+  document.getElementById('add-form').onsubmit = function(el) {
+    console.log('form submitted', el);
+    addBookmark({
+      _id: '',
+      title: document.getElementById('title').value,
+      url: document.getElementById('url').value
+    });
+
     return false;
   };
+
+  var addBookmark = function(bookmark) {
+    console.log('add', bookmark);
+    db.put(bookmark, function(err) {
+      if (err) {
+        console.log('error while adding %s to PouchDB', bookmark.id);
+      } else {
+        console.log('%s successfully aded to PouchDB', bookmark.id);
+      }
+    });
+  };
+
+  var allBookmark = function(callback) {
+    db.allDocs({
+      include_docs: true,
+      attachments: true
+    }, function(err, res) {
+      if (err) {
+        callback(err);
+      }
+      else {
+        callback(null, res.rows);
+      }
+    });
+  };
+
+
+  allBookmark(function(err, res) {
+    if (err !== null) {
+      console.log('error', err);
+    }
+    else {
+      console.log('new list of bookmarks', res);
+    }
+  });
+
 })();
