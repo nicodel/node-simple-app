e2e80fd0b37bb15e2fce410cdeb9a1ba1b5295e2
nicodel <nicolas.delebecque@gmail.com>
2015-05-01 19:56:12 +0200

    cleaning server.js and routes.js files

--- public/bundle.js
+++ public/bundle.js
@@ -8,121 +8,6 @@
   var Bookmarks = require('./models/bookmarks');
   Bookmarks.all(MainView.reinitBookmarkList);
 
-  // var PouchDB = require('pouchdb');
-  // var db = new PouchDB('cozy-pouch-client');
-
-/*  document.getElementById('add-form').onsubmit = function(el) {
-    console.log('form submitted', el);
-    addBookmark({
-      _id: Date.now().toString(),
-      title: document.getElementById('title').value,
-      url: document.getElementById('url').value
-    }, function(err) {
-      if (err) {
-        console.log('error', err);
-      } else {
-        reinitBookmarkList();
-      }
-    });
-    return false;
-  };*/
-
-/*  var reinitBookmarkList = function() {
-    allBookmark(function(err, res) {
-      if (err !== null) {
-        console.log('error', err);
-      } else {
-        console.log('new list of bookmarks', res);
-        displayBookmarkList(res);
-      }
-    });
-  };*/
-
-/*  var addBookmark = function(bookmark, callback) {
-    console.log('add', bookmark);
-    db.put(bookmark, function(err) {
-      if (err) {
-        console.log('Error while adding to PouchDB', err);
-        callback(err);
-      } else {
-        console.log('Successfully added to PouchDB', bookmark.id);
-        callback(false);
-      }
-    });
-  };*/
-
-/*  var allBookmark = function(callback) {
-    db.allDocs({
-      include_docs: true,
-      attachments: true
-    }, function(err, res) {
-      if (err) {
-        callback(err, null);
-      }
-      else {
-        console.log('got', res);
-        callback(null, res.rows);
-      }
-    });
-  };*/
-
-/*  var deleteBookmark = function(element) {
-    var bookmark = element.target.parentElement.getElementsByClassName('title')[0];
-    console.log('bookmark to delete', bookmark);
-    db.get(bookmark.id, function(err, doc) {
-      if (err) {
-        console.log('error while retreiving', err);
-      } else {
-        db.remove(doc, function(err, res) {
-          if (err) {
-            console.log('error while removing', err);
-          } else {
-            console.log('success removing', res);
-            reinitBookmarkList();
-          }
-        });
-      }
-    });
-  };*/
-
-/*  var displayBookmarkList = function(bookmarks) {
-    var ul = document.getElementById('bookmarks-list');
-    ul.innerHTML = '';
-    for (var i = 0; i < bookmarks.length; i++) {
-      var bookmark = bookmarks[i].doc;
-      console.log('bookmark', bookmark);
-      var li = document.createElement('li');
-      var a_title = document.createElement('a');
-      a_title.href = bookmark.url;
-      a_title.innerHTML = bookmark.title;
-      a_title.id= bookmark._id;
-      a_title.className = 'title';
-      li.appendChild(a_title);
-      var span1 = document.createElement('span');
-      span1.innerHTML = ' - ( ';
-      li.appendChild(span1);
-      var a_delete = document.createElement('a');
-      a_delete.href = '#';
-      a_delete.innerHTML = 'delete';
-      a_delete.addEventListener('click', deleteBookmark);
-      li.appendChild(a_delete);
-      var span2 = document.createElement('span');
-      span2.innerHTML = ' )';
-      li.appendChild(span2);
-      ul.appendChild(li);
-    }
-  };*/
-
-/*  allBookmark(function(err, res) {
-    if (err !== null) {
-      console.log('error', err);
-    }
-    else {
-      console.log('new list of bookmarks', res);
-      displayBookmarkList(res);
-    }
-  });*/
-
 })();
 
 },{"./models/bookmarks":2,"./views/main":4}],2:[function(require,module,exports){
@@ -259,7 +144,6 @@ var MainView = function() {
     ul.innerHTML = '';
     for (var i = 0; i < bookmarks.length; i++) {
       var bookmark = bookmarks[i].doc;
-      // console.log('bookmark', bookmark, i);
       var li = document.createElement('li');
       var a_title = document.createElement('a');
       a_title.href = bookmark.url;
--- server/routes.js
+++ server/routes.js
@@ -4,9 +4,7 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 
-var Bookmark = require('./bookmark');
-// var sqlite3 = require('sqlite3').verbose();
-// var db = new sqlite3.Database('cozy-split.db');
+// var Bookmark = require('./bookmark');
 
 router.use(function(req, res, next) {
   'use strict';
@@ -15,52 +13,10 @@ router.use(function(req, res, next) {
 });
 router.use(bodyParser.urlencoded({extended: true}));
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   'use strict';
   res.render('index.jade', function(err, html) {
     res.status(200).send(html);
   });
-/*  Bookmark.all(function(err, data) {
-    if (err !== null) {
-      next(err);
-    } else {
-      console.log('rendering', data);
-      res.render('index.jade', {bookmarks: data}, function(err, html) {
-        res.status(200).send(html);
-      });
-    }
-  });*/
 });
-
-router.post('/add', function(req, res, next) {
-  'use strict';
-  var bookmark = {
-    _id: req.body.title,
-    url: req.body.url,
-    title: req.body.title,
-  };
-  Bookmark.add(bookmark, function(err) {
-    console.log('Router - add', bookmark);
-    if (err !== null) {
-      next(err);
-    } else {
-      res.redirect('back');
-    }
-  });
-});
-
-router.get('/delete/:id', function(req, res, next) {
-  'use strict';
-  var id = req.params.id;
-  console.log('Router - removing', id);
-  Bookmark.remove(id, function(err) {
-    console.log('Router - remove %s - %s', id, err);
-    if (err !== null) {
-      next(err);
-    } else {
-      res.redirect('back');
-    }
-  });
-});
-
 module.exports = router;
